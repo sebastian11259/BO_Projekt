d25f89207a5634ca1e3e594a7a7d274eebc7c345
BartoszStoklosa <battruss2@gmail.com>
2023-01-17 14:49:05 +0100

    przykąłdowy plik testowy

--- test.py
+++ test.py
@@ -1,32 +1,72 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import structures
 import pandas as pd
 import os
 import numpy as np
-from random import sample
+import SA_algorithm as alg
+import matplotlib.pyplot as plt
+import time
+import structures
 
+# temper = np.linspace(100000, 100000000, 100)
+temper = [[1, 2, 3]]
+print(temper)
 
-Ttable = structures.TimeTable(r"D:\Studia\5 semestr\BO2\BO_Projekt\Dane\teachers.xlsx", r"D:\Studia\5 semestr\BO2\BO_Projekt\Dane\classes.xlsx", 10)
-Ttable.load_years(r"D:\Studia\5 semestr\BO2\BO_Projekt\Dane\Klasy")
-Ttable.initial_2()
-Ttable.to_excel(r"D:\Studia\5 semestr\BO2\BO_Projekt\Dane\plan.xlsx")
+alpha = 0.95
+eps = 0.01
+k = 10
+initial = 3
+neigh = [1]
+weigh = [100] * 6
+weigh[1] = 100
+temp = 100000
+timer = []
+y = []
+iter = []
+init_value = []
+pre_1 = [99999999999]
+pre_2 = [99999999999]
+for id, diff in enumerate(temper):
+    ins_timer = []
+    ins_y = []
+    ins_iter = []
+    ins_init_value = []
+    for i in range(2):
+        try:
+            Ttable = structures.TimeTable(r"C:\Users\Bartosz\Desktop\BO\rb\BO_Projekt\Dane\teachers.xlsx",
+                                          r"C:\Users\Bartosz\Desktop\BO\rb\BO_Projekt\Dane\classes.xlsx", 10)
+            Ttable.load_years(r"C:\Users\Bartosz\Desktop\BO\rb\BO_Projekt\Dane\Klasy")
+            start = time.time()
+            z, x, c, v = alg.sa_algorithm(temp, alpha, eps, k, Ttable, initial, diff, weigh)
+            print(v[-1], pre_2[-1])
+            if v[-1] < pre_2[-1]:
+                pre_1 = c
+                pre_2 = v
+            ins_init_value.append(v[0])
+            end = time.time()
+            ins_timer.append(end - start)
+            ins_y.append(v[-1])
+            ins_iter.append(len(v))
+            print(end - start)
+        except:
+            print("failed")
+    try:
+        print(sum(ins_init_value) / len(ins_init_value))
+        print(diff, sum(ins_iter) / len(ins_iter), sum(ins_timer) / len(ins_timer), sum(ins_y) / len(ins_y))
+        iter.append(sum(ins_iter) / len(ins_iter))
+        timer.append(sum(ins_timer) / len(ins_timer))
+        y.append(sum(ins_y) / len(ins_y))
+    except:
+        None
 
+Ttable.to_excel(r"C:\Users\Bartosz\Desktop\BO\rb\BO_Projekt\plik.xlsx")
+def create_plot2(obj_fun_current_vec, obj_fun_end):
+    plt.plot(obj_fun_current_vec, linewidth=0.5)
+    plt.plot(obj_fun_end, linewidth=0.5)
+    plt.title("Object function")
+    plt.xlabel("Iteration")
+    plt.ylabel("Obj fun value")
+    plt.legend(["Current", "Best"])
+    plt.grid()
+    plt.savefig('example.png')
+    plt.show()
 
-# print(Ttable.all_classes())
-# print(Ttable.all_teachers())
-# print(Ttable.all_years())
-# print(Ttable.table[0][0][0][0][1])
-# Ttable.table[0][0][0][0][1] = 1
-# Ttable.table[0][0][0][1][1] = 2
-# Ttable.table[0][0][1][2][1] = 2
-#
-# print(Ttable)
-# print(Ttable.many_teachers())
-
-
-
-
-# print(Ttable.finishing_time())
-# Ttable.table[0][0][0]
+create_plot2(pre_1, pre_2)
